fb314212e922fc5b7ad2945d25ee188867ddda43
gleaman38 <gleaman38@hotmail.com>
2026-03-27 15:51:27 -0400

    Added new entry

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -9,7 +9,8 @@ namespace MagicVilla_VillaAPI.Data
         public static List<VillaDTO> villaList = new List<VillaDTO>
         {
             new VillaDTO { Id = 1, Name = "Pool View", Sqft=100, Occupancy=4 },
-            new VillaDTO { Id = 2, Name = "Beach View", Sqft=300, Occupancy=3 }
+            new VillaDTO { Id = 2, Name = "Beach View", Sqft=300, Occupancy=3 },
+            new VillaDTO { Id = 3, Name = "Beach View 2", Sqft=300, Occupancy=3 }
         };
     }
 }
